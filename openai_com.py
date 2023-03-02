2503a8cae8408b83dfe70ab9d6874ed6f899d18a
georg480 <georg.dahmen@gmx.de>
2023-03-02 14:55:34 +0100

    sync openai einrichten

--- openai_com.py
+++ openai_com.py
@@ -1,13 +1,23 @@
-import asyncio
+# import asyncio
 import os
-import shutil
-from datetime import datetime
 
 import openai
 from dotenv import load_dotenv
 
+# import shutil
+# from datetime import datetime
+
+
 load_dotenv()
 API_KEY = os.getenv("OPENAI_API_KEY")
+
+
+# def chat(prompt):
+#     completions = openai.Completion.create(
+#         model="text-davinci-003", prompt=prompt, max_tokens=2048, api_key=API_KEY
+#     )
+#     message = completions.choices[0].text
+#     return message
 
 
 def chat(prompt):
@@ -19,13 +29,4 @@ def chat(prompt):
 
 
 result = chat("Schreib mir einen weihnachtlichen Text. in deutsch")
-print(result)
-
-
-# async def chat(prompt):
-#     completions = openai.Completion.create(
-#         model="text-davinci-003", prompt=prompt, max_tokens=2048, api_key=API_KEY
-#     )
-#     message = completions.choices[0].text
-#     return message
-#
+#print(result)
