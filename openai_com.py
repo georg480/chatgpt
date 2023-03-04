f9189a4d36d91b95f1b54bf097657d2fcb6b060a
georg480 <georg.dahmen@gmx.de>
2023-03-04 16:10:13 +0100

    Einfühen zum beschreiben des quellcodes

--- openai_com.py
+++ openai_com.py
@@ -1,15 +1,18 @@
 import os
-from random import randint
+from datetime import datetime
 
 import openai
 from dotenv import load_dotenv
 
-load_dotenv()
+from models.dateien import schreibe_protokol
 
+load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 def chat(anweisung, model):
+    zeit_aktuell = datetime.now().strftime("%Y.%m.%d_%H:%M:%S")
+    schreibe_protokol("Protokoll.txt", f"zu gpt {zeit_aktuell}: {anweisung}\n")
     antwort = openai.Completion.create(
         model=model,
         prompt=anweisung,
@@ -19,11 +22,6 @@ def chat(anweisung, model):
         frequency_penalty=0,
         presence_penalty=0.6,
     )
+    schreibe_protokol("Protokoll.txt", f"von gpt {zeit_aktuell}: {antwort}\n")
+    print(f"antwort: {antwort}")
     return antwort.choices[0].text
-
-
-def play_random():
-    num = randint(0, 10)
-    if num > 5:
-        return "größer"
-    return "kleiner"
