a09dfece2a541a54fe74b23b9ead74a0d67cfba6
georg480 <georg.dahmen@gmx.de>
2023-03-04 15:04:39 +0100

    englischer text kann übersetzt werdn.

--- main.py
+++ main.py
@@ -16,16 +16,21 @@ print(f"antwort: {antwort}")
 
 
 while True:
-        benutzer_eingabe = input("Frage /Eingabe ein. a(aus)")
+        benutzer_eingabe = input("Frage /Eingabe ein. a(aus), u (Eng->D)")
 
         if benutzer_eingabe.lower() == "a":
             break
-        elif benutzer_eingabe.lower() == "e":
-            anweisung = benutzer_eingabe
+        elif benutzer_eingabe.lower() == "u":
+            benutzer_eingabe = input("Englischer Text")
+            anweisung = f"Translate this into German:\n\n{benutzer_eingabe}\n\n1."
             schreibe_protokol("Protokoll.txt", f"zu gpt {zeit_aktuell}: {anweisung}\n")
             antwort = chat(anweisung, model)
             schreibe_protokol("Protokoll.txt", f"von gpt {zeit_aktuell}: {antwort}\n")
             print("Protokoll.txt", f"von gpt {zeit_aktuell}: {antwort}\n")
+
+#prompt="Translate this into German:\n\nWhat rooms do you have available?\n\n1.",
+
+
 # async def main():
 #     context = ""
 #
