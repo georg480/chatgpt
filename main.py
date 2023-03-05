433c48d42881089c520a7597ceb8026e7e68b415
georg480 <georg.dahmen@gmx.de>
2023-03-05 08:40:39 +0100

    konstante Model groß geschreiben

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 from openai_com import chat
 
-model = "text-davinci-003"
+MODEL = "text-davinci-003"
 
 while True:
     benutzer_eingabe = input("Frage /Eingabe ein. x(aus), e (erkläre Code), u (Eng->D)")
 
-    if benutzer_eingabe.lower() == "x":
-        break
-    elif benutzer_eingabe.lower() == "u":
+    if benutzer_eingabe.lower() == "u":
         anweisung = (
-            f"Translate this into German:\n\n" + input("Englischer Text") + "\n\n"
+            "Translate this into German:\n\n" + input("Englischer Text") + "\n\n"
         )
-        antwort = chat(anweisung, model)
+        antwort = chat(anweisung, MODEL)
         print(f"\n{antwort}")
     elif benutzer_eingabe.lower() == "e":
         anweisung = (
-            f"Erkläre die Funktion des Codes:\n\n" + input("Eingabe Quellcode") + "\n\n"
+            "Erkläre die Funktion des Codes:\n\n" + input("Eingabe Quellcode") + "\n\n"
         )
-        antwort = chat(anweisung, model)
+        antwort = chat(anweisung, MODEL)
         print(f"\n{antwort}")
     elif benutzer_eingabe.lower() == "c":
         anweisung = input("Eingabe Frage")
-        antwort = chat(anweisung, model)
+        antwort = chat(anweisung, MODEL)
         print(f"\n{antwort}")
+    elif benutzer_eingabe.lower() == "x":
+        break
     else:
         print(benutzer_eingabe)
