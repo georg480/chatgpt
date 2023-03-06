dfa0532fd7d6372b3a4b59c83821f7256cdf60d9
georg480 <georg.dahmen@gmx.de>
2023-03-06 09:53:20 +0100

    bezeichnung von you auf du geädert

--- gpt3.py
+++ gpt3.py
@@ -1,19 +1,26 @@
 import os
+
 import openai
-
-
-
 from dotenv import load_dotenv
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-#openai.api_key = os.getenv('OPENAI_KEY')
+# openai.api_key = os.getenv('OPENAI_KEY')
 
 
-def gpt3(prompt, engine='text-davinci-003', response_length=64,
-         temperature=0.7, top_p=1, frequency_penalty=0, presence_penalty=0,
-         start_text='', restart_text='', stop_seq=[]):
+def gpt3(
+    prompt,
+    engine="text-davinci-003",
+    response_length=64,
+    temperature=0.7,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0,
+    start_text="",
+    restart_text="",
+    stop_seq=[],
+):
     response = openai.Completion.create(
         prompt=prompt + start_text,
         engine=engine,
@@ -24,6 +31,6 @@ def gpt3(prompt, engine='text-davinci-003', response_length=64,
         presence_penalty=presence_penalty,
         stop=stop_seq,
     )
-    answer = response.choices[0]['text']
+    answer = response.choices[0]["text"]
     new_prompt = prompt + start_text + answer + restart_text
     return answer, new_prompt
--- komm_chat.py
+++ komm_chat.py
@@ -6,17 +6,18 @@ def chat():
 AI: I'm good! What would you like to chat about?
 Human:"""
     while True:
-        prompt += input('You: ')
-        answer, prompt = gpt3(prompt,
-                              temperature=0.9,
-                              frequency_penalty=1,
-                              presence_penalty=1,
-                              start_text='\nAI:',
-                              restart_text='\nHuman: ',
-                              stop_seq=['\nHuman:', '\n'])
-        print('GPT-3:' + answer)
+        prompt += input("Du: ")
+        answer, prompt = gpt3(
+            prompt,
+            temperature=0.9,
+            frequency_penalty=1,
+            presence_penalty=1,
+            start_text="\nAI:",
+            restart_text="\nHuman: ",
+            stop_seq=["\nHuman:", "\n"],
+        )
+        print("GPT-3:" + answer)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     chat()
-
