3b2118941577561a11f8f11bf9a954ce310d22bf
georg480 <georg.dahmen@gmx.de>
2023-03-08 10:45:19 +0100

    Erklaäre - Erkläre

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 from komm_chat import komm_chat
 from openai_com import chat, erzeuge_unittest
+from models.dateien import lese_datei,schreibe_protokol
 
 MODEL = "text-davinci-003"
 
@@ -10,11 +11,13 @@ while True:
         "Eingabe c(chat), e(erkl.Code), f(Frage), t(Unittest), u(E->D), x(en)"
     )
     if eingabe_benutzer.lower() == "u":
+        print(lese_datei("englischer_text.txt"))
         anweisung = (
-            "Translate this into German:\n\n" + input("Englischer Text") + "\n\n"
+            "Translate this into German:\n\n" + lese_datei("englischer_text.txt") + "\n\n"
         )
         antwort = chat(anweisung, MODEL, 150)
         print(f"\n{antwort}")
+        schreibe_protokol("deutscher_text.txt", antwort)
     elif eingabe_benutzer.lower() == "c":
         komm_chat()
     elif eingabe_benutzer.lower() == "t":
--- models/dateien.py
+++ models/dateien.py
@@ -1,6 +1,11 @@
 import os
-
-
 def schreibe_protokol(datei_namen, inhalt_einfuegen):
     with open(datei_namen, "a", encoding="utf-8") as file:
         file.write(inhalt_einfuegen)
+
+
+def lese_datei(datei_namen):
+    with open(datei_namen, "r", encoding="utf-8") as file:
+        datei_inhalt = file.read()
+    return datei_inhalt
+
