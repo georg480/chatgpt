3095bf7e9e9067060cc236886bfd6dd0a43326e6
georg480 <georg.dahmen@gmx.de>
2023-03-09 13:27:11 +0100

    test mainpy

--- test/test_main.py
+++ test/test_main.py
@@ -8,11 +8,9 @@ class TestSkript(unittest.TestCase):
     def test_erzeuge_unittest(self):
         skripname = "test_skript"
         model = "text-davinci-003"
-        self.assertEqual(erzeuge_unittest(skripname, model), "Skript erfolgreich getestet")
+        self.assertEqual(
+            erzeuge_unittest(skripname, model), "Skript erfolgreich getestet"
+        )
 
     def test_komm_chat(self):
         self.assertEqual(komm_chat(), "Chat erfolgreich beendet")
-
-if __name__ == '__main__':
-  unittest.main()
-  '\n
