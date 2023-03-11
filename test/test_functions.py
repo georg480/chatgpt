485b495119e5638f2dd26a2a79b020338b1019c8
georg.dahmen@gmx.de <georg.dahmen@gmx.de>
2023-03-11 09:18:17 +0100

    In diesem Unittest wird die gpt3 Funktion getestet, indem ein Mock für die openai.Completion.create Funktion erstellt wird, die von der gpt3 Funktion aufgerufen wird. Der Unittest überprüft, ob die Mock-Funktion korrekt aufgerufen wurde und ob die Ausgabe der gpt3 Funktion den erwarteten Werten entspricht.

--- test/test_functions.py
+++ test/test_functions.py
@@ -1,7 +1,7 @@
+import os
 import subprocess
 import unittest
 from unittest.mock import patch
-import os
 
 from models.functions import pruefe_py_gebaut
 
@@ -10,7 +10,11 @@ class TestFunctions(unittest.TestCase):
     @patch("subprocess.call")
     def test_pruefe_py_gebaut(self, mock_subprocess_call):
         skript_name = "main.py"
-        with patch.object(os, "getcwd", return_value=r'C:\Users\georg\Seafile\Meine Bibliothek-008100\Dokumente\documents\programmieren\PycharmProjects\chatgpt'):
+        with patch.object(
+            os,
+            "getcwd",
+            return_value=r"C:\Users\georg\Seafile\Meine Bibliothek-008100\Dokumente\documents\programmieren\PycharmProjects\chatgpt",
+        ):
             pruefe_py_gebaut(skript_name)
 
         mock_subprocess_call.assert_any_call("isort .", shell=True)
--- test/test_gpt3.py
+++ test/test_gpt3.py
@@ -1,61 +1,35 @@
-# import os
-# import unittest
-#
-# import openai
-# from dotenv import load_dotenv
-# from gpt3 import gpt3
-#
-# class TestGpt3(unittest.TestCase):
-#     def setUp(self):
-#         load_dotenv()
-#         openai.api_key = os.getenv("OPENAI_API_KEY")
-#
-#     def test_gpt3_default(self):
-#         prompt = "This is a test prompt: "
-#         answer, new_prompt = gpt3(prompt)
-#
-#         self.assertNotEqual(answer, None)
-#         self.assertIsInstance(answer, str)
-#         self.assertEqual(new_prompt, prompt + answer)
-#
-#     def test_gpt3_temperature(self):
-#         prompt = "This is a test prompt: "
-#         temperature = 0.2
-#
-#         answer, new_prompt = gpt3(prompt, temperature=temperature)
-#
-#         self.assertNotEqual(answer, None)
-#         self.assertIsInstance(answer, str)
-#         self.assertEqual(new_prompt, prompt + answer)
-#
-#     def test_gpt3_top_p(self):
-#         prompt = "This is a test prompt: "
-#         top_p = 0.9
-#
-#         answer, new_prompt = gpt3(prompt, top_p=top_p)
-#
-#         self.assertNotEqual(answer, None)
-#         self.assertIsInstance(answer, str)
-#         self.assertEqual(new_prompt, prompt + answer)
-#
-#     def test_gpt3_frequency_penalty(self):
-#         prompt = "This is a test prompt: "
-#         frequency_penalty = 2.0
-#
-#         answer, new_prompt = gpt3(prompt, frequency_penalty=frequency_penalty)
-#
-#         self.assertNotEqual(answer, None)
-#         self.assertIsInstance(answer, str)
-#         self.assertEqual(new_prompt, prompt + answer)
-#
-#     def test_gpt3_presence_penalty(self):
-#         prompt = "This is a test prompt: "
-#         presence_penalty = 0.7
-#
-#         answer, new_prompt = gpt3(prompt, presence_penalty=presence_penalty)
-#
-#         self.assertNotEqual(answer, None)
-#         self.assertIsInstance(answer, str)
-#         self.assertEqual(new_prompt, prompt + answer)
+import unittest
+from unittest.mock import patch
+from models.gpt3 import gpt3
 
 
+class TestGpt3(unittest.TestCase):
+    @patch("models.gpt3.openai.Completion.create")
+    @patch("models.gpt3.os.getenv")
+    def test_gpt3(self, mock_getenv, mock_completion_create):
+        mock_getenv.return_value = "test_api_key"
+        mock_response = {
+            "choices": [{"text": "This is a test response."}],
+        }
+        mock_completion_create.return_value = mock_response
+
+        prompt = "This is a test prompt."
+        expected_answer = "This is a test response."
+        expected_new_prompt = prompt + expected_answer
+
+        answer, new_prompt = gpt3(prompt)
+
+        mock_getenv.assert_called_with("OPENAI_API_KEY")
+        mock_completion_create.assert_called_with(
+            prompt=prompt,
+            engine="text-davinci-003",
+            max_tokens=512,
+            temperature=0.7,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=[],
+        )
+
+        self.assertEqual(answer, expected_answer)
+        self.assertEqual(new_prompt, expected_new_prompt)
