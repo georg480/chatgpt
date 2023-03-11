bcf56ef95914d027ffaf4d2f315e373d675bd15e
georg.dahmen@gmx.de <georg.dahmen@gmx.de>
2023-03-11 08:55:05 +0100

    Dieser Unittest verwendet das unittest-Modul und den Dekorator patch aus dem unittest.mock-Modul. Es gibt einen Test test_pruefe_py_gebaut, der die Funktion pruefe_py_gebaut testet.

--- test/test_functions.py
+++ test/test_functions.py
@@ -1,8 +1,20 @@
-import pytest
+import unittest
+import subprocess
+import os
+from unittest.mock import patch
+from models.functions import pruefe_py_gebaut
 
-from models.functions import mal, minus, plus, teilen
+class TestFunctions(unittest.TestCase):
 
+    @patch('subprocess.call')
+    def test_pruefe_py_gebaut(self, mock_subprocess_call):
+        skript_name = "test_script.py"
+        with patch('models.functions.os') as mock_os:
+            mock_os.getcwd.return_value = '/path/to/test/directory'
+            pruefe_py_gebaut(skript_name)
 
-def test_plus():
-    result = plus(3, 1)
-    assert result == 4
+        mock_subprocess_call.assert_called_with("isort .", shell=True)
+        mock_subprocess_call.assert_called_with("black .", shell=True)
+        mock_subprocess_call.assert_called_with("pylint " + skript_name, shell=True)
+        mock_subprocess_call.assert_called_with("pyreverse -a1 -s1  -f ALL -d ALL  .  -o pdf", shell=True)
+        mock_subprocess_call.assert_called_with("pytest", shell=True)
