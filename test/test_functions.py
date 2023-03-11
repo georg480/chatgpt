613a5a5dec2c30337214dab033a08e06b32dbab3
georg.dahmen@gmx.de <georg.dahmen@gmx.de>
2023-03-11 09:13:26 +0100

    Hier haben wir os importiert und patch.object() verwendet, um die getcwd-Funktion zu patchen. Außerdem wurde assert_called_with() durch assert_any_call() ersetzt, da subprocess.call() mehrmals aufgerufen wird.

--- test/test_functions.py
+++ test/test_functions.py
@@ -1,20 +1,22 @@
-import unittest
 import subprocess
-import os
+import unittest
 from unittest.mock import patch
+import os
+
 from models.functions import pruefe_py_gebaut
 
-class TestFunctions(unittest.TestCase):
 
-    @patch('subprocess.call')
+class TestFunctions(unittest.TestCase):
+    @patch("subprocess.call")
     def test_pruefe_py_gebaut(self, mock_subprocess_call):
-        skript_name = "test_script.py"
-        with patch('models.functions.os') as mock_os:
-            mock_os.getcwd.return_value = '/path/to/test/directory'
+        skript_name = "main.py"
+        with patch.object(os, "getcwd", return_value=r'C:\Users\georg\Seafile\Meine Bibliothek-008100\Dokumente\documents\programmieren\PycharmProjects\chatgpt'):
             pruefe_py_gebaut(skript_name)
 
-        mock_subprocess_call.assert_called_with("isort .", shell=True)
-        mock_subprocess_call.assert_called_with("black .", shell=True)
-        mock_subprocess_call.assert_called_with("pylint " + skript_name, shell=True)
-        mock_subprocess_call.assert_called_with("pyreverse -a1 -s1  -f ALL -d ALL  .  -o pdf", shell=True)
-        mock_subprocess_call.assert_called_with("pytest", shell=True)
+        mock_subprocess_call.assert_any_call("isort .", shell=True)
+        mock_subprocess_call.assert_any_call("black .", shell=True)
+        mock_subprocess_call.assert_any_call("pylint " + skript_name, shell=True)
+        mock_subprocess_call.assert_any_call(
+            "pyreverse -a1 -s1  -f ALL -d ALL  .  -o pdf", shell=True
+        )
+        mock_subprocess_call.assert_any_call("pytest", shell=True)
