8b46e3d0aaa57205efdfe2130402d2d9f3987ecf
georg.dahmen@gmx.de <georg.dahmen@gmx.de>
2023-03-11 08:51:23 +0100

    nicht genutze Funktionen plus, minus, mal und teilen gelöscht

--- models/functions.py
+++ models/functions.py
@@ -8,19 +8,3 @@ def pruefe_py_gebaut(skript_name: str):
     subprocess.call("pyreverse -a1 -s1  -f ALL -d ALL  .  -o pdf", shell=True)
     subprocess.call("pytest", shell=True)
     print("Suche nach ERROR und entferne am Ende das init, Fehler und prüfe den Import")
-
-
-def plus(in1: int, in2: int):
-    return in1 + in2
-
-
-def minus(in1: int, in2: int):
-    return in1 - in2
-
-
-def teilen(in1: int, in2: int):
-    return in1 / in2
-
-
-def mal(in1: int, in2: int):
-    return in1 * in2
