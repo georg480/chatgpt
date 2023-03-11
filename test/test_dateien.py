8fbfdd258b5d9056815275eac9c2b0d18e3f8e8e
georg.dahmen@gmx.de <georg.dahmen@gmx.de>
2023-03-11 08:49:17 +0100

    unittest für models/dateien.py hinzugefügt

--- test/test_dateien.py
+++ test/test_dateien.py
@@ -1,20 +1,33 @@
 import os
+import tempfile
 import unittest
 
-from models.dateien import schreibe_protokol
+from models.dateien import lese_datei, schreibe_datei, schreibe_protokol
 
 
 class TestDateien(unittest.TestCase):
     def setUp(self):
-        self.testdatei = "testdatei.txt"
-        with open(self.testdatei, "w", encoding="utf-8") as file:
-            file.write("Dies ist eine Testdatei.")
+        self.test_dir = tempfile.mkdtemp()
+        self.test_file = os.path.join(self.test_dir, "test.txt")
 
     def tearDown(self):
-        os.remove(self.testdatei)
+        os.remove(self.test_file)
+        os.rmdir(self.test_dir)
 
     def test_schreibe_protokol(self):
-        schreibe_protokol(self.testdatei, "Neuer Inhalt.")
-        with open(self.testdatei, "r", encoding="utf-8") as file:
-            inhalt = file.read()
-        self.assertEqual(inhalt, "Dies ist eine Testdatei. Neuer Inhalt.")
+        schreibe_protokol(self.test_file, "Test Protokoll")
+        with open(self.test_file, "r") as file:
+            content = file.read()
+            self.assertEqual(content, "Test Protokoll")
+
+    def test_lese_datei(self):
+        with open(self.test_file, "w") as file:
+            file.write("Testinhalt\nzweite Zeile")
+        content = lese_datei(self.test_file)
+        self.assertEqual(content, "Testinhalt zweite Zeile")
+
+    def test_schreibe_datei(self):
+        schreibe_datei(self.test_file, "Testinhalt")
+        with open(self.test_file, "r") as f:
+            content = f.read()
+            self.assertEqual(content, "Testinhalt")
