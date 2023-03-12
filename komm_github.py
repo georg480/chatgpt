ed284b95392a642ee1fa86ceb83d8e961869c4c1
georg.dahmen@gmx.de <georg.dahmen@gmx.de>
2023-03-12 10:53:12 +0100

    Hallo Max Mustermann,
    Ich hoffe, es geht Ihnen gut. Hier ist eine Zusammenfassung Ihrer Bestellung:
    
    2x Produkt A (10 Euro pro St�ck)
    
    1x Produkt B (5 Euro pro St�ck)
    
    3x Produkt C (8 Euro pro St�ck)
    
    Vielen Dank f�r Ihre Bestellung!
    
    Mit freundlichen Gr��en,
    Ihr Name

---
+++ komm_github.py
@@ -0,0 +1,19 @@
+from jinja2 import Environment, FileSystemLoader
+
+# Definieren Sie die Variablen
+name = "Max Mustermann"
+order = [
+    {"name": "Produkt A", "quantity": 2, "price": 10},
+    {"name": "Produkt B", "quantity": 1, "price": 5},
+    {"name": "Produkt C", "quantity": 3, "price": 8},
+]
+your_name = "Ihr Name"
+
+# Erstellen Sie die Jinja2-Umgebung und laden Sie die Template-Datei
+env = Environment(loader=FileSystemLoader('.'))
+template = env.get_template('template.j2')
+
+# Rendern Sie das Template mit den Variablen und speichern Sie es in einer Datei
+output = template.render(name=name, order=order, your_name=your_name)
+with open('commit_template.txt', 'w') as file:
+    file.write(output)
--- test/test_openai_com.py
+++ test/test_openai_com.py
@@ -10,7 +10,7 @@ class TestOpenAI(TestCase):
     @mock.patch("openai.Completion.create")
     def test_erzeuge_unittest(self, mock_create):
         mock_create.return_value.choices[0].text = (
-            "import unittest\n\nclass TestMyClass(TestCase):\n"
+            "import unittest\n\nclass TestMyClass(unittest.TestCase):\n"
             "    def test_my_function(self):\n"
             "        self.assertEqual(1 + 1, 2)\n"
         )
@@ -22,14 +22,14 @@ class TestOpenAI(TestCase):
         erzeuge_unittest(skript_quelle, model)
 
         expected_content = (
-            "import unittest\n\nclass TestMyClass(TestCase):\n"
+            "import unittest\n\nclass TestMyClass(unittest.TestCase):\n"
             "    def test_my_function(self):\n"
             "        self.assertEqual(1 + 1, 2)\n"
         )
 
-        with open(f"test/test_{skript_quelle}.py", "r") as f:
+        with open(f"test/test_{skript_quelle}", "r") as f:
             actual_content = f.read()
 
-        os.remove(f"test/test_{skript_quelle}.py")
+        os.remove(f"test/test_{skript_quelle}")
 
         self.assertEqual(actual_content, expected_content)
