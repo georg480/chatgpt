9213978fe32b7624b9e0bfac1ca70b62c160d534
georg.dahmen@gmx.de <georg.dahmen@gmx.de>
2023-03-16 09:10:58 +0100

    Ablaufdiagramme eingefügt

--- komm_revchatgpt.py
+++ komm_revchatgpt.py
@@ -10,11 +10,14 @@ from models.functions import pruefe_py_gebaut
 
 conf = json.load(open("chatgpt.json"))
 
-uuid = None
+convo_id = "46f4b87e-8fcd-43fc-9b8d-97845fdc7f74"
+parent_id = "5d6b43fe-1042-428e-8614-4c476fd1c694"
 
-chatbot = Chatbot(config=conf, conversation_id=uuid, parent_id=uuid)
-# anweisung = input("Stellen Sie eine Frage: ")
+chatbot = Chatbot(config=conf, conversation_id=convo_id, parent_id=parent_id)
+# anweisung = input("Stellen Sie eine Frage: ") + ',"text-davinci-003", 4096'
+# print(anweisung)
 # antwort = list(chatbot.ask(anweisung))
+# print(f"antwort liste:{antwort}")
 # antwort = antwort[-1]['message']
 # print(antwort)
 
@@ -23,6 +26,28 @@ def chat(anweisung, model, max_laenge):
     zeit_aktuell = datetime.now().strftime("%Y.%m.%d_%H:%M:%S")
     schreibe_protokol("protokoll.txt", f"zu gpt {zeit_aktuell}: {anweisung}\n")
     antwort = list(chatbot.ask(anweisung))
+
+    get_conversations = chatbot.get_conversations(offset=0, limit=20, encoding=None)
+    print(get_conversations)
+    convo_id = antwort[-1]["conversation_id"]
+    print(f"convo_id: {convo_id}")
+    parent_id = antwort[-1]["parent_id"]
+    print(f"parent_id: {parent_id}")
+
+    get_msg_history = chatbot.get_msg_history(convo_id=convo_id, encoding=None)
+    print(get_msg_history)
+#    id = antwort[-1]["id"]
+ #   print(f"id: {id}")
+#    gen_title = chatbot.gen_title(convo_id=convo_id, message_id="TEST Titel")
+    #change_title= chatbot.change_title(convo_id=convo_id,title="Change")
+    #delete_conversation= chatbot.delete_conversation(convo_id=convo_id)
+    #print(f"delete_conversation: {delete_conversation}")
+    #clear_conversations=chatbot.clear_conversations()
+    #print(f"clear_conversations: {clear_conversations}")
+    #reset_chat = chatbot.reset_chat()
+    #print(f"reset_chat: {reset_chat}")
+    #rollback_conversation = chatbot.rollback_conversation(num=1)
+    #print(f"rollback_conversation: {rollback_conversation}")
     antwort = antwort[-1]["message"]
     schreibe_protokol("protokoll.txt", f"von gpt {zeit_aktuell}: {antwort}\n")
     print(f"Antwort: {antwort}")
--- uml/komm_chat-benutzer.md.backup
+++
@@ -1,9 +0,0 @@
-sequenceDiagram
-participant Benutzer
-participant OpenAI API
-participant komm_chat() Funktion
-
-    Benutzer->>komm_chat(): gibt Eingabeaufforderung ein
-    komm_chat()->>OpenAI API: sendet Eingabeaufforderung an API
-    OpenAI API-->>komm_chat(): sendet Antwort zurück
-    komm_chat()-->>Benutzer: gibt Antwort aus
