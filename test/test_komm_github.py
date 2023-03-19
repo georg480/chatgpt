472dfaf5bab49af44220bb1b453a07e368eed064
Georg Dahmen <georg.dahmen@gmx.de>
2023-03-19 17:18:05 +0100

    - __pycache__/test_komm_github.cpython-311-pytest-7.2.1.pyc - test_komm_github.py - ../main.py - ../models/namensmodel.py
    Funktion hinzufügen.

--- test/test_komm_github.py
+++ test/test_komm_github.py
@@ -2,7 +2,7 @@ import os.path
 import subprocess
 import unittest
 import sys
-
+print(os.getcwd())
 import komm_github
 
 
@@ -10,13 +10,15 @@ class TestGitCommit(unittest.TestCase):
     def test_git_commit(self):
         komm_github.git_commit()
         print(os.getcwd())
-        self.assertTrue(os.path.isfile("commit_template.txt"))
+        datei_name="commit_template.txt"
 
-        with open("../commit_template_test.txt", "r", encoding="utf-8", newline="\n") as f:
-            template = f.read()
-            expected_template = "- ../commit_template.txt\n\n- test/test_komm_github.py\n\nFunktion hinzufügen."
-            self.assertEqual(template.strip(), expected_template)
+        self.assertTrue(os.path.isfile(datei_name))
+        #
+        with open(datei_name, "r", encoding="utf-8", newline="\n") as f:
+              template = f.read()
+              expected_template = 'test\\----commit_template__txt' #"- "+datei_name+"\n\n- test/test_komm_github.py\n\nFunktion hinzufügen."
+              self.assertEqual(template.strip(), expected_template)
 
-        subprocess.call(["git", "reset", "--hard"])
-        subprocess.call(["git", "clean", "-f"])
-        os.remove("../commit_template.txt")
+        # subprocess.call(["git", "reset", "--hard"])
+        # subprocess.call(["git", "clean", "-f"])
+      #  os.remove(datei_name)
