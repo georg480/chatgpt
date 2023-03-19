00b65ca3052522814d6f129d43123a8dbfef23d2
Georg Dahmen <georg.dahmen@gmx.de>
2023-03-19 11:01:27 +0100

    - __pycache__/komm_revchatgpt.cpython-311.pyc - commit_template.txt - test/__pycache__/test_komm_github.cpython-311-pytest-7.2.1.pyc - test/test_komm_github.py
    Funktion hinzufügen.

--- test/test_komm_github.py
+++ test/test_komm_github.py
@@ -7,19 +7,15 @@ import komm_github
 
 class TestGitCommit(unittest.TestCase):
     def test_git_commit(self):
-        # Ausführung der Funktion
         komm_github.git_commit()
 
-        # Überprüfung, ob die Commit-Template-Datei erstellt wurde
         self.assertTrue(os.path.isfile("commit_template.txt"))
 
-        # Überprüfung des Inhalts der Commit-Template-Datei
         with open("commit_template.txt", "r", encoding="utf-8") as f:
             template = f.read()
             expected_template = "- komm_github.py\n\nFunktion hinzufügen."
             self.assertEqual(template.strip(), expected_template)
 
-        # Bereinigung der Testumgebung
         subprocess.call(["git", "reset", "--hard"])
         subprocess.call(["git", "clean", "-f"])
         os.remove("commit_template.txt")
