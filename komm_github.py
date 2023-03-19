96db151b57a69e0ef8d4ea7f43ec24e8efa236d6
Georg Dahmen <georg.dahmen@gmx.de>
2023-03-19 10:08:25 +0100

    - commit_template.txt - komm_github.py - models/__pycache__/dateien.cpython-311.pyc - models/__pycache__/functions.cpython-311.pyc - test/__pycache__/test_dateien.cpython-311-pytest-7.2.1.pyc - test/__pycache__/test_functions.cpython-311-pytest-7.2.1.pyc - test/__pycache__/test_main.cpython-311-pytest-7.2.1.pyc - test/__pycache__/test_openai_com.cpython-311-pytest-7.2.1.pyc
    Add feature X and fix bug Y

--- komm_github.py
+++ komm_github.py
@@ -1,30 +1,26 @@
 import subprocess
 
-# Führe git status aus und erfasse die Ausgabe
+# Füge alle geänderten Dateien dem Index hinzu
+subprocess.call(["git", "add", "."])
+
+# Erfasse die geänderten Dateien und erstelle eine automatische Commit-Nachricht
 output = subprocess.check_output(["git", "status"])
-
-# Dekodiere die Ausgabe von Bytes zu String
 output = output.decode("utf-8")
-
-# Teile die Ausgabe in Zeilen auf
 lines = output.split("\n")
-
-# Initialisiere eine leere Liste für die Commit-Nachrichten-Zeilen
 commit_message = []
-
-# Schleife durch die Zeilen und suche nach geänderten Dateien
 for line in lines:
-    # Wenn die Zeile mit "modified:" beginnt, füge den Dateinamen zur Commit-Nachricht hinzu
     if line.startswith("\tmodified:"):
-        # Entferne das Präfix "modified:" und entferne alle Leerzeichen
         file_name = line.replace("modified:", "").strip()
-        # Füge einen Aufzählungspunkt mit dem Dateinamen zur Commit-Nachricht hinzu
         commit_message.append(f"- {file_name}")
-
-# Verbinde die Commit-Nachrichten-Zeilen mit Zeilenumbrüchen
 commit_message = "\n".join(commit_message)
 
-# Öffne die Vorlagendatei im Anhängemodus
-with open("commit_template.txt", "a") as f:
-    # Schreibe die Commit-Nachricht in die Datei
-    f.write(commit_message)
+# Definiere die Commit-Beschreibung und Autor-Information
+commit_description = "Add feature X and fix bug Y"
+author_info = "Georg Dahmen <georg.dahmen@gmx.de>"
+
+# Schreibe die Commit-Nachricht und die Commit-Beschreibung in die Vorlagendatei
+with open("commit_template.txt", "w") as f:
+    f.write(commit_message + "\n\n" + commit_description)
+
+# Erstelle einen Commit mit der automatisch generierten Commit-Nachricht, der Commit-Beschreibung und der Autor-Information
+subprocess.call(["git", "commit", "-F", "commit_template.txt", "--author=" + author_info])
