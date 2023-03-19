3345f3f98a2eb88481a9b70638794758eff627e1
Georg Dahmen <georg.dahmen@gmx.de>
2023-03-19 13:27:05 +0100

    - __pycache__/test_komm_github.cpython-311-pytest-7.2.1.pyc - test_komm_github.py
    Funktion hinzufügen.

--- test/test_komm_github.py
+++ test/test_komm_github.py
@@ -8,14 +8,13 @@ import komm_github
 class TestGitCommit(unittest.TestCase):
     def test_git_commit(self):
         print(os.getcwd())
-        os.remove("commit_template.txt")
         komm_github.git_commit()
 
-        self.assertTrue(os.path.isfile("commit_template.txt"))
+        self.assertTrue(os.path.isfile("../commit_template.txt"))
 
-        with open("commit_template.txt", "r", encoding="utf-8", newline="\n") as f:
+        with open("../commit_template.txt", "r", encoding="utf-8", newline="\n") as f:
             template = f.read()
-            expected_template = "- commit_template.txt\n\n- test/test_komm_github.py\n\nFunktion hinzufügen."
+            expected_template = "- ../commit_template.txt\n\n- test/test_komm_github.py\n\nFunktion hinzufügen."
             self.assertEqual(template.strip(), expected_template)
 
         subprocess.call(["git", "reset", "--hard"])
